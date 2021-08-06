bb6daea93d6198753c9aa7d07062a984dc433358
Federico Llano <Llanofederico@gmail.com>
2021-08-06 10:34:39 -0300

    se agrega cupones y estilos en css

--- descuentos.js
+++ descuentos.js
@@ -15,3 +15,43 @@ calcularDescuento = () => {
     const resultP = document.getElementById("container-result");
     resultP.innerHTML = `El precio con el descuento es $${result}.`;
 }
+
+const inputCoupon = document.getElementById("InputCoupon");
+const couponValue = inputCoupon.value;
+
+onClickButtonPriceDiscount = () => {
+    const inputPrice = document.getElementById("precio");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    const coupons = [
+        {
+            name: "15%OFF",
+            discount: 15,
+        },
+        {
+            name: "30%OFF",
+            discount: 30,
+        },
+        {
+            name: "25%OFF",
+            discount: 25,
+        },
+    ];
+    isCouponValueValid = coupon => coupon.name === couponValue;
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + userCouponValue + "no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultP = document.getElementById("container-result");
+        resultP.innerHTML = `El precio con el descuento es $${precioConDescuento}.`;
+    }
+
+}
